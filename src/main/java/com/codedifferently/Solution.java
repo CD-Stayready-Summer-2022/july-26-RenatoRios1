7bd4d57074cff1b19a6f7af4a546f6a3a70f8895
RenatoRios1 <renatoarios1@gmail.com>
2022-07-26 08:28:25 -0500

    done

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,23 @@
 package com.codedifferently;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        TreeMap<Character, Integer> treeMap = new TreeMap<>();
+        for(int i = 0;  i < input.length(); i++){
+            if(!treeMap.containsKey(input.charAt(i))){
+                treeMap.put(input.charAt(i), 1);
+            }else{
+                treeMap.put(input.charAt(i), treeMap.get(input.charAt(i))+1);
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Map.Entry<Character, Integer> entry:  treeMap.entrySet()){
+            stringBuilder.append(String.format("%c:%d ", entry.getKey(), entry.getValue()));
+        }
+        return stringBuilder.substring(0, stringBuilder.length()-1);
+
     }
 }
